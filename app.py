e6ee5559666d2a76a24c1b323ae65dcbeba2d877
Goh Jun Yi <gohjunyi00@gmail.com>
2022-07-09 23:59:08 +0800

    feat: two class model ready

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
 import io
 import json
 
+import torch
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
 
+MODEL_PATH = r'models\densenet_two_resize_299.model'
+LABEL_MAP_PATH = r'two_label_map.json'
+INPUT_SIZE = 224
 
 app = Flask(__name__)
-imagenet_class_index = json.load(open('imagenet_label_map.json'))
-model = models.densenet121(pretrained=True)
+
+imagenet_class_index = json.load(open(LABEL_MAP_PATH))
+model = torch.load(MODEL_PATH)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
 model.eval()
 
-
 def transform_image(image_bytes):
-    my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
-    image = Image.open(io.BytesIO(image_bytes))
-    return my_transforms(image).unsqueeze(0)
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(INPUT_SIZE),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(INPUT_SIZE),
+            transforms.CenterCrop(INPUT_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    return data_transforms['val'](image).unsqueeze(0)
 
 
 def get_prediction(image_bytes):
     tensor = transform_image(image_bytes=image_bytes)
+    tensor = tensor.to(device)
     outputs = model.forward(tensor)
-    _, y_hat = outputs.max(1)
-    predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+
+    probs = torch.nn.functional.softmax(outputs, dim=1)
+    conf, classes = torch.max(probs, 1)
+    predicted_idx = str(classes.item())
+    conf = conf.item()
+
+    return conf, imagenet_class_index[predicted_idx]
 
 
 @app.route('/predict', methods=['POST'])
@@ -37,8 +56,8 @@ def predict():
     if request.method == 'POST':
         file = request.files['file']
         img_bytes = file.read()
-        class_id, class_name = get_prediction(image_bytes=img_bytes)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
+        conf, class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'confidence': conf, 'class_name': class_name})
 
 
 if __name__ == '__main__':
