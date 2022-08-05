ec16b88f8183d38007067425a2740e3039dc8634
Goh Jun Yi <gohjunyi00@gmail.com>
2022-08-05 18:37:25 +0800

    feat: object detection support (pretrained models only)

--- app.py
+++ app.py
@@ -2,45 +2,59 @@ import io
 import json
 
 import torch
-from torchvision import models
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
 import torchvision.transforms as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
 
-MODEL_TYPE = 0 # 0 -> Image Classification, 1 -> Object Detection
-MODEL_PATH = 'models/densenet_full_resize_199_cpu.model'
-LABEL_MAP_PATH = 'label_maps/full_label_map.json'
-INPUT_SIZE = 224
+data_transforms = lambda input_size : {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(input_size),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
-app = Flask(__name__)
+#### CONFIGURATIONS ####
 
-label_map = json.load(open(LABEL_MAP_PATH))
-model = torch.load(MODEL_PATH)
+## OBJECT DETECTION
+MODEL_TYPE = 1 # 0 -> Image Classification, 1 -> Object Detection
+MODEL = fasterrcnn_resnet50_fpn_v2(weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT, box_score_thresh=0.9)
+TRANSFORM = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT.transforms() 
+LABEL_MAP = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT.meta["categories"] 
+
+## IMAGE CLASSIFICATION
+# INPUT_SIZE = 224
+# MODEL_TYPE = 0
+# MODEL = 'models/densenet_full_resize_199_cpu.model'
+# TRANSFORM = data_transforms(INPUT_SIZE)['val']
+# LABEL_MAP = 'label_maps/full_label_map.json' 
+
+#### CONFIGURATIONS ####
+
+label_map = json.load(open(LABEL_MAP)) if type(LABEL_MAP) == str else LABEL_MAP
+model = torch.load(MODEL) if type(MODEL) == str else MODEL
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 model.eval()
 
-def transform_image(image_bytes):
-    data_transforms = {
-        'train': transforms.Compose([
-            transforms.RandomResizedCrop(INPUT_SIZE),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]),
-        'val': transforms.Compose([
-            transforms.Resize(INPUT_SIZE),
-            transforms.CenterCrop(INPUT_SIZE),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]),
-    }
+app = Flask(__name__)
+
+def preprocess_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    return data_transforms['val'](image).unsqueeze(0)
+
+    return TRANSFORM(image).unsqueeze(0)
 
 
 def get_classifcation_prediction(image_bytes):
-    tensor = transform_image(image_bytes=image_bytes)
+    tensor = preprocess_image(image_bytes=image_bytes)
     tensor = tensor.to(device)
     outputs = model.forward(tensor)
 
@@ -51,6 +65,22 @@ def get_classifcation_prediction(image_bytes):
 
     return conf, label_map[predicted_idx]
 
+def get_object_detection_prediction(image_bytes):
+    tensor = preprocess_image(image_bytes=image_bytes)
+
+    tensor = tensor.to(device)
+    outputs = model.forward(tensor)[0]
+    boxes, labels, scores = outputs['boxes'], outputs['labels'], outputs['scores']
+    
+    objects = []
+    for i in range(len(boxes)):
+        objects.append({
+            'box': boxes[i].tolist(),
+            'class_name': label_map[labels[i].item()],
+            'confidence': scores[i].item()
+        })
+
+    return objects
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -62,6 +92,9 @@ def predict():
         if MODEL_TYPE == 0:
             conf, class_name = get_classifcation_prediction(image_bytes=img_bytes)
             res = jsonify({'confidence': conf, 'class_name': class_name})
+        elif MODEL_TYPE == 1:
+            objects = get_object_detection_prediction(image_bytes=img_bytes)
+            res = jsonify({'result': objects})
         else:
             return jsonify({'message': 'Unknown model type configuration'}), 500
         
